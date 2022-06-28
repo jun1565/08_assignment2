216e9e7b11d2b94719c9621713b73aa1de5927d4
jun1565 <jun156521@gmail.com>
2022-06-29 03:25:40 +0900

    create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,19 @@
+n = 2   #入力させる数値の数
+input_number = []  #入力した数値を格納する配列
+
+for i in range(n):
+    input_number += [int(input(str(i+1) + "番目の数値入力 "))] #入力した数値をxに格納
+
+
+#四則演算
+#和
+print('足し算の計算結果:{}'.format(input_number[0] + input_number[1]))
+
+#差
+print('引き算の計算結果:{}'.format(input_number[0] - input_number[1]))
+
+#積
+print('掛け算の計算結果:{}'.format(input_number[0] * input_number[1]))
+
+#商
+print('割り算の計算結果:{}'.format(input_number[0] / input_number[1]))
